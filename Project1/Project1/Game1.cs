a428f7e274857e5641965bf0e0d4e8265630f3bd
Gruson Nathalie <nathalie.gruson@univ-smb.fr>
2022-01-03 17:27:40 +0100

    changement de titre

--- Project1/Project1/Game1.cs
+++ Project1/Project1/Game1.cs
@@ -19,6 +19,7 @@ namespace Project1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            Window.Title = "Test";
 
             base.Initialize();
         }
